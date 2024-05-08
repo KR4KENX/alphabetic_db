4eedb133cd4cb71fd04e6e3d7316036431a165af
sqat <sqat1024@gmail.com>
2024-05-08 10:08:49 +0200

    Linear search through db, fix for sorting in v

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <string>
 using namespace std;
+
 bool biggerWord(string word1, string word2){
+    for (char &c : word1)
+        c = tolower(c);
+    for (char &c : word2)
+        c = tolower(c);
     // Word 1 > Word 2 = return True, Word 2 >= Word 1 = return False
     for(int i = 0; i < max(word1.size(), word2.size()); i++){
         if (i >= word1.size()) return false;
@@ -13,29 +19,54 @@ bool biggerWord(string word1, string word2){
     }
     return false;
 }
+bool compareIgnoringCapital(char a, char b) {
+    return std::tolower(a) < std::tolower(b);
+}
+bool linearSearchDB(ifstream& db, int r, string target){
+    int l = 0;
+    string line;
+    while (l < r){
+        getline(db, line);
+        if (line == target) return true;
+        if (biggerWord(line, target)) return false;
+        l++;
+    }
+    return false;
+}
+int checkFileLenght(ifstream& db, int new_words_count){
+    string line;
+    int line_count;
+    getline(db, line);
+    line_count = stoi(line);
+    return line_count + new_words_count;
+}
 void write(ifstream& db, ofstream& temp_db, vector<string>& words){
     int w_iterator = 0;
     string line;
+    temp_db << to_string(checkFileLenght(db, words.size())) << '\n';
     while(getline(db, line)){
         if (line.empty()) break;
         if(w_iterator < words.size()){
             bool words_compare = biggerWord(line, words[w_iterator]);
-            cout << words_compare << " : " << line << " : " << words[w_iterator] << endl;
             while(w_iterator < words.size() && words_compare){
-                cout << "Inner loop";
                 temp_db << words[w_iterator] << '\n';
                 w_iterator++; 
                 if (w_iterator >= words.size()) break;
                 words_compare = biggerWord(line, words[w_iterator]);
             }
         }
-            temp_db << line << '\n';
+        temp_db << line << '\n';
     }
     while(w_iterator < words.size()){
         temp_db << words[w_iterator] << '\n';
         w_iterator++;
     }
 }
+bool read(ifstream& db, string word){
+    int line_count = checkFileLenght(db, 0);
+    return linearSearchDB(db, line_count, word);
+}
+
 
 int main(int argc, char* argv[]){
     ofstream ou("dbdata/temp");
@@ -56,12 +87,19 @@ int main(int argc, char* argv[]){
             words.push_back(word);
         }
         sort(words.begin(), words.end());
+        sort(words.begin(), words.end(), [](const std::string& a, const std::string& b) {
+            return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end(), compareIgnoringCapital);
+        });
         write(in, ou, words);
         remove(argv[2]);
         rename("dbdata/temp", argv[2]);
     }
     if(argv[1][0] == 'r'){
         cout << "Read from database ... " << endl;
+        string word;
+        getline(cin, word);
+        if (read(in, word)) cout << "Word already in DB" << endl;
+        else cout << "Word not in DB" << endl;
     }
     in.close();
     ou.close();
